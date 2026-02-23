7fa7437637e3c64a2a65ddd688e2361ab27a85fe
Moltschun <daniilsvetashov@gmail.com>
2026-02-23 13:24:41 +0700

    feat: start implementing core device logic and UART command processing in main

---
+++ Servodrive/src/main.c
@@ -0,0 +1,27 @@
+#include "servo.h"
+#include "uart_driver.h"
+#include "sys.core.h"
+
+void main(void){
+
+    RCC_System_72HZ();
+
+    UART_Init();
+
+    Servo_Init();
+
+    UART_String("Начала исследования...\r\n");
+
+    while (1)
+    {
+        UART_String("Введите канал (1-2): ");
+        while(!rx_flag);
+        uint8_t ch = rx_buffer;
+        rx_flag = 0;
+        UART_String("Введите угол поворота (0-180): ");
+        while(!rx_flag);
+        uint8_t ang = rx_buffer;
+        rx_flag = 0;
+        Servo_Turn(ch, ang);
+    }    
+}
